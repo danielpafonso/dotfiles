ee2d70b8d2e0b131328cb23731277b5ecdce6ddc
Daniel Afonso <daniel.afonso@parceiros.nos.pt>
2024-06-25 09:07:51 +0100

    Seperate status line from theme

--- configs/neovim/lazy-nvim/lua/plugins/colorscheme.lua
+++ configs/neovim/lazy-nvim/lua/plugins/colorscheme.lua
@@ -11,22 +11,6 @@ return {
     end,
   },
   {
-    -- Set lualine as statusline
-    "nvim-lualine/lualine.nvim",
-    opts = {
-      options = {
-        icons_enabled = true,
-        theme = "auto",
-        component_separators = "|",
-      },
-      inactive_sections = {
-        lualine_a = {"filename"},
-        lualine_c = {"filesize"},
-        lualine_x = {},
-      },
-    },
-  },
-  {
     "catppuccin/nvim",
     -- config = function()
     --   vim.cmd.colorscheme "catppuccin-macchiato"
