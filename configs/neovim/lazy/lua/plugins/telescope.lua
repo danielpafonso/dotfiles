07a37381855c59306fb5053553237ca272e682d5
Daniel Afonso <daniel.afonso@parceiros.nos.pt>
2025-01-08 10:27:56 +0000

    formating

--- configs/neovim/lazy/lua/plugins/telescope.lua
+++ configs/neovim/lazy/lua/plugins/telescope.lua
@@ -4,7 +4,7 @@ return {
   branch = "0.1.x",
   dependencies = {
       "nvim-lua/plenary.nvim",
-      { "nvim-telescope/telescope-fzf-native.nvim", build = "make"},
+      { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
       -- "nvim-tree/nvim-web-devicons",
   },
   -- Only load if `make` is available. Make sure you have the system requirements installed.
@@ -15,12 +15,15 @@ return {
   config = function()
     local telescope = require("telescope")
     local actions = require("telescope.actions")
-	local builtin = require('telescope.builtin')
+	  local builtin = require('telescope.builtin')
 
     telescope.setup({
       defaults = {
         --path_display = { "smart" },
         path_display = { "truncate" },
+        file_ignore_patterns = {
+          ".git/",
+        },
         mappings = {
           i = {
             ["<C-k>"] = actions.move_selection_previous, -- move to prev result
@@ -34,14 +37,14 @@ return {
 
     -- set keymaps
     --vim.keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "[F]uzzy [f]ind files in cwd" })
-	vim.keymap.set("n", "<leader>ff", function() builtin.find_files({ hidden = true,  no_ignore = true }) end, { desc = "[F]uzzy [f]ind files in cwd" })
+    vim.keymap.set("n", "<leader>ff", function() builtin.find_files({ hidden = true,  no_ignore = true }) end, { desc = "[F]uzzy [f]ind files in cwd" })
     vim.keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
     vim.keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "[F]uzzy find [s]tring in cwd" })
     vim.keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "[F]uzzy find string under [c]ursor in cwd" })
  
-	vim.keymap.set("n", "<leader>ft", function()
-		builtin.colorscheme({ enable_preview = true, ignore_builtins = true })
-	end, {})
+    vim.keymap.set("n", "<leader>ft", function()
+      builtin.colorscheme({ enable_preview = true, ignore_builtins = true })
+    end, {})
     vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = "[F]uzzy find existing [b]uffers" })
     vim.keymap.set('n', '<leader>/', function()
       -- You can pass additional configuration to telescope to change theme, layout, etc.
