c603e17085096652948ca805f87b3f29d1b5cbd6
Daniel Afonso <daniel.afonso@parceiros.nos.pt>
2025-01-28 14:10:07 +0000

    Add Treesitter plugin

---
+++ configs/neovim/lazy/lua/plugins/treesitter.lua
@@ -0,0 +1,41 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  event = { "BufReadPre", "BufNewFile" },
+  build = ":TSUpdate",
+  config = function()
+    -- import nvim-treesitter plugin
+    local treesitter = require("nvim-treesitter.configs")
+
+    -- configure treesitter
+    treesitter.setup({ -- enable syntax highlighting
+      highlight = {
+        enable = true,
+      },
+      -- enable indentation
+      indent = { enable = true },
+      -- ensure these language parsers are installed
+      ensure_installed = {
+        "dockerfile",
+        "go",
+        "json",
+        "markdown",
+        "markdown_inline",
+        "python",
+        "terraform",
+        "yaml",
+        -- "make",
+        -- "vrl",
+      },
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          init_selection = "<C-space>",
+          node_incremental = "<C-space>",
+          scope_incremental = false,
+          node_decremental = "<bs>",
+        },
+      },
+    })
+  end,
+}
+
