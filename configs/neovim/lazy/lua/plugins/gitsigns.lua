b9ecb4c83f36f9e8f8051ecb1316d5f15bc0375d
Daniel Afonso <daniel.afonso@parceiros.nos.pt>
2025-01-28 14:17:30 +0000

    Add navigation to git signs

--- configs/neovim/lazy/lua/plugins/gitsigns.lua
+++ configs/neovim/lazy/lua/plugins/gitsigns.lua
@@ -15,6 +15,10 @@ return {
       local function map(mode, l, r, desc)
         vim.keymap.set(mode, l, r, { buffer = bufnr, desc = desc })
       end
+      -- Navigation
+      map("n", "]h", gs.next_hunk, "Next Hunk")
+      map("n", "[h", gs.prev_hunk, "Prev Hunk")
+      -- Actions
       map("n", "<leader>hS", gs.stage_buffer, "Stage buffer")
       map("n", "<leader>hR", gs.reset_buffer, "Reset buffer")
       map("n", "<leader>hu", gs.undo_stage_hunk, "Undo stage hunk")
@@ -27,7 +31,6 @@ return {
       map("n", "<leader>hD", function()
         gs.diffthis("~")
       end, "Diff this ~")
-
       -- Text object:
       map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>", "Gitsigns select hunk")
     end,
