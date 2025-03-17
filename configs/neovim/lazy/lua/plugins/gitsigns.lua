44fe9b32b1c0ef84f4c5a2b9efab6436e752aaca
Daniel Afonso <daniel.afonso@parceiros.nos.pt>
2025-03-17 10:30:44 +0000

    Add gitsigns keymap

--- configs/neovim/lazy/lua/plugins/gitsigns.lua
+++ configs/neovim/lazy/lua/plugins/gitsigns.lua
@@ -21,6 +21,7 @@ return {
       -- Actions
       map("n", "<leader>hS", gs.stage_buffer, "Stage buffer")
       map("n", "<leader>hR", gs.reset_buffer, "Reset buffer")
+      map("n", "<leader>hr", gs.reset_hunk, "Reset hunk")
       map("n", "<leader>hu", gs.undo_stage_hunk, "Undo stage hunk")
       map("n", "<leader>hp", gs.preview_hunk, "Preview hunk")
       map("n", "<leader>hb", function()
